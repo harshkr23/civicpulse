6ce306e270acf5f467507c92a84548f66b2ad1b2
Harsh <harshkrchy138@gmail.com>
2026-08-23 15:38:39 +0530

    first

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://civicpulse-3-yxue.onrender.com',
         changeOrigin: true,
         secure: false,
       },
